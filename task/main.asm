5b9cf3a6c68da68706c1aa9f5370f4966f6ea124
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-05-04 10:39:43 -0300

    feat: finish task algorithm

--- task/main.asm
+++ task/main.asm
@@ -17,26 +17,35 @@ main:
   blt $t0, 2, rangeError
 
   li $t6, 2
+
   j loopUntilNumber
   
 loopUntilNumber:
-  move $t1, $t0
+  move $t1, $t6
   li $t7, 2
   jal isPrime
+
 
   beq $t2, 1, foundPrime
 
   add $t6, $t6, 1
+
   blt $t6, $t0, loopUntilNumber
+  bgt $t5, 0, showPrime
   j notFound
 
 foundPrime:
+  move $t5, $t6
+  add $t6, $t6, 1
+  j loopUntilNumber
+
+showPrime:
   li $v0, 4
   la $a0, msgResponseNumber
   syscall
 
+  move $a0, $t5
   li $v0, 1
-  move $a0, $t6
   syscall
   j end
 
@@ -62,7 +71,7 @@ isPrime:
 
 isPrimeLoop:
   rem $t3, $t1, $t7
-  #beq $t3, 0, isMultiple
+  beq $t3, 0, isMultiple
   add $t7, $t7, 1
   blt $t7, $t1, isPrimeLoop
   j isPrime
