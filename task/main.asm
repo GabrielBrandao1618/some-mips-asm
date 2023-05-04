b0833361e876ba0ea65beda3f154741a9732ef2c
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-05-03 22:07:43 -0300

    feat: start task

---
+++ task/main.asm
@@ -0,0 +1,74 @@
+.data
+  msgRequestNumber: .asciiz"\nInsira um numero entre 2 e 50000: "
+  msgResponseNumber: .asciiz"\nO primeiro numero primo anterior: "
+  msgErrorRange: .asciiz"\nERRO: numero deve ser entre 2 e 50000"
+  msgErrorNotFound: .asciiz"\nERRO: numero primo nao encontrado"
+.text
+main:
+  li $v0, 4
+  la $a0, msgRequestNumber
+  syscall
+
+  li $v0, 5
+  syscall
+  move $t0, $v0
+
+  bgt $t0, 50000, rangeError
+  blt $t0, 2, rangeError
+
+  li $t6, 2
+  j loopUntilNumber
+  
+loopUntilNumber:
+  move $t1, $t0
+  li $t7, 2
+  jal isPrime
+
+  beq $t2, 1, foundPrime
+
+  add $t6, $t6, 1
+  blt $t6, $t0, loopUntilNumber
+  j notFound
+
+foundPrime:
+  li $v0, 4
+  la $a0, msgResponseNumber
+  syscall
+
+  li $v0, 1
+  move $a0, $t6
+  syscall
+  j end
+
+notFound:
+  la $a0, msgErrorNotFound
+  li $v0, 4
+  syscall
+  j end
+  
+rangeError:
+  li $v0, 4
+  la $a0, msgErrorRange
+  syscall
+
+  j main
+
+# Recebe um número pelo t1, e seu retorno é pelo t2
+isPrime:
+  blt $t7, $t1, isPrimeLoop
+  # Se após o loop, nenhum divisor foi encontrado, então o número é primo
+  li $t2, 1
+  jr $ra
+
+isPrimeLoop:
+  rem $t3, $t1, $t7
+  #beq $t3, 0, isMultiple
+  add $t7, $t7, 1
+  blt $t7, $t1, isPrimeLoop
+  j isPrime
+
+isMultiple:
+  li $t2, 0
+  jr $ra
+
+end:
